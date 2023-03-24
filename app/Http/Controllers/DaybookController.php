3113a43c71af8b0233e38aa86f875c9c937c6982
NabinNS <nabinshrestha348@gmail.com>
2023-03-24 23:13:31 +0545

    daybook user control summary display complete

--- app/Http/Controllers/DaybookController.php
+++ app/Http/Controllers/DaybookController.php
@@ -23,29 +23,40 @@ class DaybookController extends Controller
 
         $mergedDetails = $partyDetails->union($customerDetails);
         $sortedDetails = $mergedDetails->orderBy('date')->paginate(10);
-        return view('daybook.daybook', compact('sortedDetails', 'startDate', 'endDate','totalPurchase','totalCashPaid','totalSales','totalCashReceived'));
+        return view('daybook.daybook', compact('sortedDetails', 'startDate', 'endDate', 'totalPurchase', 'totalCashPaid', 'totalSales', 'totalCashReceived'));
     }
     public function viewDayBook(Request $request)
     {
         $startDate = $request->from;
         $endDate = $request->to;
+        $totalPurchase = 0;
+        $totalCashPaid = 0;
+        $totalSales = 0;
+        $totalCashReceived = 0;
         if ($request->option == "All") {
             $partyDetails = AccountLedger::whereBetween('date', [$startDate, $endDate]);
             $customerDetails = CustomerLedger::whereBetween('date', [$startDate, $endDate]);
+
+            $totalPurchase = $partyDetails->sum('credit');
+            $totalCashPaid = $partyDetails->sum('debit');
+            $totalSales = $customerDetails->sum('debit');
+            $totalCashReceived = $customerDetails->sum('credit');
+
             $mergedDetails = $partyDetails->union($customerDetails);
             $sortedDetails = $mergedDetails->orderBy('date')->paginate(10);
-            $sortedDetails->appends(['from' => $startDate, 'to' => $endDate]);
-        }
-        elseif($request->option == "Purchase"){
+        } elseif ($request->option == "Purchase") {
             $mergedDetails = AccountLedger::whereBetween('date', [$startDate, $endDate]);
             $sortedDetails = $mergedDetails->orderBy('date')->paginate(10);
             $sortedDetails->appends(['from' => $startDate, 'to' => $endDate]);
-        }
-        else{
+            $totalPurchase = $mergedDetails->sum('credit');
+            $totalCashPaid = $mergedDetails->sum('debit');
+        } else {
             $mergedDetails = CustomerLedger::whereBetween('date', [$startDate, $endDate]);
             $sortedDetails = $mergedDetails->orderBy('date')->paginate(10);
             $sortedDetails->appends(['from' => $startDate, 'to' => $endDate]);
+            $totalSales = $mergedDetails->sum('debit');
+            $totalCashReceived = $mergedDetails->sum('credit');
         }
-        return view('daybook.daybook', compact('sortedDetails', 'startDate', 'endDate'));
+        return view('daybook.daybook', compact('sortedDetails', 'startDate', 'endDate', 'totalPurchase', 'totalCashPaid', 'totalSales', 'totalCashReceived'));
     }
 }
