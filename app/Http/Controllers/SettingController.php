0f7d2809187ee1f6fcb4f158b0b4e5d0faf0ca74
NabinNS <nabinshrestha348@gmail.com>
2023-03-26 20:57:58 +0545

    update user setting complete

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -13,5 +13,23 @@ class SettingController extends Controller
         $user = User::find($id);
         return view('settings.setting', compact('user'));
     }
-  
+    public function updateUser($id, Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'password' => 'required',
+            'location' => 'required',
+            'email' => 'required',
+            'phone' => 'required',
+        ]);
+        $user = User::find($id);
+        $user->update([
+            'name'=> $request->name,
+            'password' => Hash::make($request->password),
+            'location' => $request->location,
+            'email' => $request->email,
+            'phone_number' => $request->phone,
+        ]);
+        return redirect('setting/' . $id)->with('success', 'User update successful');
+    }
 }
